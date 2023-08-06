dee0c1a5b2031461d6a24efb4f25e6bc9ebbff8d
londonreturns <subedioscar.47@gmail.com>
2023-08-06 08:28:41 +0545

    php db connection

---
+++ connection.php
@@ -0,0 +1,11 @@
+<?php
+	$hostname = "localhost";
+	$username = "root";
+	$password = "";
+	$dbname = "weather-app";
+
+	$conn = mysqli_connect($hostname, $username, $password, $dbname);
+
+	if ($conn->connect_error) {
+    	die("Connection failed: " . $conn->connect_error);
+	}
